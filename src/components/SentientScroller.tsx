87949ea951ef8d804a78bc9fe9d6a1890a9ec9d5
kyrers <kyrers.eth@gmail.com>
2023-07-10 22:29:36 +0100

    Users can now define a minimum difference between the current and previous positions for a new position to be stored

--- src/components/SentientScroller.tsx
+++ src/components/SentientScroller.tsx
@@ -3,12 +3,14 @@ import { StyledButton } from "./styles";
 import { ScrollIcon } from "./Icons/ScrollIcon";
 
 type SentientScrollerProps = PropsWithChildren & {
-  threshold: number;
+  timeThreshold: number;
+  positionThreshold?: number;
 };
 
 export default function SentientScroller({
   children,
-  threshold,
+  timeThreshold,
+  positionThreshold = 0,
 }: SentientScrollerProps) {
   const [storedPositions, setStoredPositions] = useState<number[]>([]);
 
@@ -18,11 +20,24 @@ export default function SentientScroller({
     window.clearTimeout(timeout);
     timeout = window.setTimeout(() => {
       const currentPosition = window.scrollY;
-      setStoredPositions((storedPositions) => [
-        ...storedPositions,
-        currentPosition,
-      ]);
-    }, threshold);
+
+      setStoredPositions((storedPositions) => {
+        const [previousPosition] = storedPositions.slice(-1);
+
+        /**
+         * Checking if previousPosition is undefined causes errors when previousPosition is 0.
+         * This is because 0 is a falsy value in javascript, meaning it would be considered undefined...
+         */
+        if (
+          storedPositions.length < 2 ||
+          Math.abs(currentPosition - previousPosition) > positionThreshold
+        ) {
+          return [previousPosition, currentPosition];
+        } else {
+          return storedPositions;
+        }
+      });
+    }, timeThreshold);
   };
 
   const scrollToStoredPosition = () => {
