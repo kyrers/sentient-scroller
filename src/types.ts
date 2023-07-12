6ab3ead5b6ac569be6413bf55053dc1d4fba468a
kyrers <kyrers.eth@gmail.com>
2023-07-12 21:44:09 +0100

    Added description to props

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,23 @@
 import { PropsWithChildren } from "react";
 
 export type SentientScrollerProps = PropsWithChildren & {
+  /**
+   * The threshold (in milliseconds) for considering a position as stable before storing it.
+   * A new position will only be stored if the user remains in the same position for longer than this threshold.
+   */
   timeThreshold: number;
+
+  /**
+   * The threshold (in pixels) for considering a position worthy of storing.
+   * A new position will only be stored if the distance to the last stored position is longer than this threshold.
+   * Defaults to 0.
+   */
   positionThreshold?: number;
+
+  /**
+   * The behavior of the scroll button.
+   * - "sequential" (default): Scrolls to the last stored position that's different from the current position.
+   * - "static": Scrolls to the first stored position that's different from the current position.
+   */
   behavior?: "sequential" | "static";
 };
